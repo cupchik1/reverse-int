0cc3440fe79e4718a1fd358297e470387c2bbad6
Alina Bezuhla <alinabezuglaya123@gmail.com>
2023-08-05 23:37:20 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    n = n < 0 ?  n * -1 : n;
+
+    let reversedNumber =  String(n).split('')
+    reversedNumber = reversedNumber.reverse();
+    reversedNumber = "".concat(...reversedNumber)
+    return reversedNumber
 }
